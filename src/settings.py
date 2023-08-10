44ca3f331c65e8199f63140f30ddcf8e65c30f1c
Arsenii Vladimirov <78355358+grokhi@users.noreply.github.com>
2023-08-10 20:13:07 +0600

    Update settings.py

--- src/settings.py
+++ src/settings.py
@@ -15,6 +15,7 @@ from dataset_tools.templates import (
 ##################################
 PROJECT_NAME: str = "Tomato Detection"
 PROJECT_NAME_FULL: str = "Tomato Detection"
+HIDE_DATASET = False  # set False when 100% sure about repo quality
 
 ##################################
 # * After uploading to instance ##
@@ -79,6 +80,7 @@ def get_settings():
     settings = {
         "project_name": PROJECT_NAME,
         "license": LICENSE,
+        "hide_dataset": HIDE_DATASET,
         "applications": APPLICATIONS,
         "category": CATEGORY,
         "cv_tasks": CV_TASKS,
